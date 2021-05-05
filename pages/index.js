f62a64457bb282d951bff525c924a955cb0c986e
Carlos Espinosa <carloespinosa88@gmail.com>
2021-05-05 16:04:30 -0500

    fix: add styles and svg's icons

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import AppLayout from '../components/AppLayout';
 import { colors } from '../styles/theme';
+import Button from '../components/Button';
+import GitHub from '../components/Icon/GitHub'
 
 export default function Home() {
   return (
@@ -8,6 +10,10 @@ export default function Home() {
       <section>
           <img src="/fish.svg" width="100" height="100" alt="devter" />
           <h1>Welcome to Devter</h1>
+          <Button>
+            <GitHub width='20' height='20' fill={`${colors.white}`} />
+            Ingresar
+          </Button>
       </section>
       <style jsx>
         {`
