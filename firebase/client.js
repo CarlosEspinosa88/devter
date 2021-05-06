cb34d67c06f63075f45495d524cd9f1708340cef
Carlos Espinosa <carloespinosa88@gmail.com>
2021-05-05 22:43:07 -0500

    fix: add firebase with github login

---
+++ firebase/client.js
@@ -0,0 +1,46 @@
+// import * as firebase from 'firebase';
+import firebase from 'firebase/app'
+import "firebase/auth"
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD8QIsNEUmkFGcJ4WUn8kayBSpKfnNjCyg",
+  authDomain: "devter-e0cd3.firebaseapp.com",
+  projectId: "devter-e0cd3",
+  storageBucket: "devter-e0cd3.appspot.com",
+  messagingSenderId: "877293946298",
+  appId: "1:877293946298:web:2024d7a8ba235c4ee80983",
+  measurementId: "G-YS7N6ZVDHH"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+
+
+const mapUserFromFirebaseAuthToUser = (user) => {
+  console.log('USER', user)
+  return {
+    avatar: user?.photoURL ,
+    username: user?.displayName,
+    email: user?.email
+  }
+}
+
+export const onAuthStateChanged = (onChange) => {
+  return firebase
+    .auth()
+    .onAuthStateChanged(user => {
+      const normalizedUser = mapUserFromFirebaseAuthToUser(user)
+      onChange(normalizedUser)
+    })
+}
+
+export const loginWithGitHub = () => {
+  const provider = new firebase.auth.GithubAuthProvider();
+
+  return firebase
+    .auth()
+    .signInWithPopup(provider)
+}
